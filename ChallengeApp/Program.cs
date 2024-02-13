ee82b52d05936361b038a69101ab9896a75e223e
Gnomek1 <kacperbab@protonmail.com>
2024-02-13 18:21:14 +0100

    Day5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Enter a number: ");
+            string number = Console.ReadLine();
+            // mogłbym pierwsze przekonwertowac tą liczbę na inta ale nie ma to chyba sensu
+            char[] numberInArray = number.ToArray();
+            for (int i = 0; i < 10; i++)
+            {
+                int counter =0;
+                foreach (var c in numberInArray)
+                {
+                    if ( i== (c - '0'))
+                    {
+                        counter++;
+                    }
+
+                }
+                Console.WriteLine($"{i}=>{counter}");
+            }
             Console.ReadLine();
         }
     }
