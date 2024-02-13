bce88e719705119d38a7ac95fb4ea279eaa027ed
Gnomek1 <kacperbab@protonmail.com>
2024-02-13 11:30:52 +0100

    Day 4

---
+++ ChallengeApp/Day4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp
+{
+    internal class Day4
+    {
+        public void Homework()
+        {
+            Console.Write("Enter your name:");
+            string Name = Console.ReadLine();
+            bool male = false;
+            int age = 30;
+            if (!male)
+            {
+                if (age < 30)
+                {
+                    Console.WriteLine("Kobieta poniżej 30 lat");
+                }
+                else if (age == 30 && Name == "Ewa")
+                {
+                    Console.WriteLine("Ewa, lat 30");
+                }
+
+            }
+            else if (male && age < 18)
+            {
+                Console.WriteLine("Niepełnoletni Mężczyzna");
+            }
+           
+           
+        }
+       
+    }
+}
