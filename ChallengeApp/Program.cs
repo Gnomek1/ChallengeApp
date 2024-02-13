9ea31c29df563c32b2519d1cf3423b5545494716
Gnomek1 <kacperbab@protonmail.com>
2024-02-13 11:12:22 +0100

    Dodaj pliki projektów.

---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChallengeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
