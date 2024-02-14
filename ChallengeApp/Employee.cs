f73f3089c2d02341417632f98f53318eafc1059c
Gnomek1 <kacperbab@protonmail.com>
2024-02-14 01:40:22 +0100

    Day 6

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp
+{
+    internal class Employee
+    {
+        public List<int> points = new List<int>();
+        public string FirstName { get; private set; }
+        public string LastName { get;  set;}
+        public int    Age { get; private set; }        
+        public int Result
+        {
+            get
+            {
+                return this.points.Sum();
+            }
+        }
+
+        public Employee(string FirstName,string LastName, int Age) 
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+        public void AddPoints(int points) 
+        {
+            if (points >=0 && points<=10) 
+            {
+                this.points.Add(points);
+            }
+            else
+            {
+                Console.WriteLine("Podaj wartość od 1 do 10");
+            }
+            
+        }
+
+
+    }
+}
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -4,12 +4,49 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 65; i <=90; i++)
-            {
-                var a = (char)i;
-                Console.Write($"{a}, ");
+            
+            Employee e1 = new Employee("Krzysztof","Kononowicz",60);
+            Employee e2 = new Employee("Wojciech","Suchodolski",70);
+            Employee e3 = new Employee("Jan","Los",50);
+            e1.AddPoints(1);
+            e1.AddPoints(10);
+            e1.AddPoints(4);
+            e1.AddPoints(6);
+            e1.AddPoints(2);
 
+            e2.AddPoints(2);
+            e2.AddPoints(10);
+            e2.AddPoints(9);
+            e2.AddPoints(8);
+            e2.AddPoints(7);
+
+            e3.AddPoints(7);
+            e3.AddPoints(2);
+            e3.AddPoints(1);
+            e3.AddPoints(10);
+            e3.AddPoints(10);
+
+            List<Employee> list = new List<Employee>();
+            list.Add(e1);
+            list.Add(e2);
+            list.Add(e3);
+
+            int maxRes = 0;
+            Employee eMaxRes = null;
+
+            foreach (var item in list)
+            {
+                if (item.Result>=maxRes) 
+                {
+                    maxRes = item.Result;
+                    eMaxRes = item;
+                }
             }
+            Console.WriteLine($"{eMaxRes.FirstName} {eMaxRes.LastName} zdobył {eMaxRes.Result} ptk.");
+
+
+
+
             Console.ReadLine();
         }
     }
