60dc9ac6cff73f749f7a4c769e2a23ce90786677
Gnomek1 <kacperbab@protonmail.com>
2024-02-15 02:57:00 +0100

    add coments to point 3 steps in test (arange, act , assert)

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -7,6 +7,7 @@ namespace ChallengeApp.Tests
         [Test]
         public void WhenEmployeeScores30Points_ThenCorrectResult()
         {
+            //arange
             var employee1 = new Employee("Peter", "Steel",5);
             employee1.AddPoints(3);
             employee1.AddPoints(5);
@@ -16,38 +17,40 @@ namespace ChallengeApp.Tests
             employee1.AddPoints(3);
             employee1.AddPoints(2);
 
-
+            //act
             int result = employee1.Result;
 
-
+            //assert
             Assert.AreEqual(30, result);
         }
         [Test]
         public void WhenEmployeeScoresNegativePoints_ThenThrowError()
         {
+            //arrange
             var employee2 = new Employee("Peter", "Steel", 10);
             employee2.AddPoints(3);
             employee2.AddPoints(5);
             employee2.AddPoints(-10);
             employee2.AddPoints(-1);
-
+            //act
             int result= employee2.Result;
 
-
+            //asset
             Assert.Negative(result);
         }
         [Test]
         public void WhenEmployeeAddAndSubtractPoints_ThenCorrectResult()
         {
+            //arrange
             var employee3 = new Employee("Peter", "Steel", 15);
             employee3.AddPoints(3);
             employee3.AddPoints(5);
             employee3.SubtractPoints(-5);
             employee3.SubtractPoints(-4);
-
+            //act
             int result = employee3.Result;
 
-
+            //assert
             Assert.AreEqual(-1,result);
         }
     }
