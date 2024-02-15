16a3b247a90847f5bd44aee2eea3d5a4dd62f206
Gnomek1 <kacperbab@protonmail.com>
2024-02-15 03:50:30 +0100

    Day 8 - reference and value type on NUNIT

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void ReferenceTypeTest()
+        {           
+            var employee1 = GetEmployee("Dead", "Mayhem");
+            var employee2 = GetEmployee("Varg", "Vikerness");            
+            Assert.AreNotEqual(employee1, employee2);         
+        }
+
+        [Test]
+        public void ValueTypeTestFloat()
+        {
+            var x = 7.5;
+            var  y= 14.5;
+            var result = x+y;
+            Assert.AreEqual(result,22);
+        }
+
+        [Test]
+        public void ValueTypeTestString()
+        {
+            var employee1 = GetEmployee("Varg", "Mayhem");
+            var employee2 = GetEmployee("Varg", "Vikerness");
+            Assert.AreEqual(employee1.FirstName,employee2.FirstName);
+        }
+        [Test]
+        public void ValueTypeTestInt()
+        {
+            int x = -1;
+            int y = -3;
+            int result = x+y;
+            Assert.Negative(result);
+        }
+
+        private Employee GetEmployee(string firstName, string lastName)
+        {
+            return new Employee(firstName, lastName);
+        }
+
+    }
+}
