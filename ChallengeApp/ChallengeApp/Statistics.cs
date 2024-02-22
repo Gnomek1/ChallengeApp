55dfe8b55c1894d95789d067c43870e5bc96042a
Gnomek1 <kacperbab@protonmail.com>
2024-02-22 21:57:01 +0100

    refactoring Statistics.cs

--- ChallengeApp/ChallengeApp/Statistics.cs
+++ ChallengeApp/ChallengeApp/Statistics.cs
@@ -8,9 +8,51 @@ namespace ChallengeApp
 {
     public class Statistics
     {
-        public float Min { get; set; }
-        public float Max { get; set; }
-        public float Average { get; set; }
-        public char AverageLetter { get; set; }
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+        public float Sum { get; private set; }
+        public int  Count { get; private set; }
+        public float Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+        public char AverageLetter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var a when a >= 80:
+                        return 'A';                        
+                    case var a when a >= 60:
+                        return 'B';
+                    case var a when a >= 40:
+                        return 'C';
+                    case var a when a >= 20:
+                        return 'D';
+                    default:
+                        return 'E';
+                }
+            }
+        }
+
+        public Statistics()
+        {
+            Count = 0;
+            Sum = 0;
+            Max = float.MinValue;
+            Min = float.MaxValue;
+        }
+
+        public void AddGrade(float points)
+        {
+            Count++;
+            Sum += points;
+            Min = Math.Min(points, Min);
+            Max = Math.Max(points, Max);
+        }
     }
 }
